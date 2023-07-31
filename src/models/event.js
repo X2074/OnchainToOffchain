3dc70e348559cd129834a2c96fe833da11f17f46
jasonzysun <jasonzysun@163.com>
2023-07-31 22:19:10 +0800

    增加索引

--- src/models/event.js
+++ src/models/event.js
@@ -4,10 +4,10 @@ const { Schema, model } = mongoose
 
 const eventSchema = new Schema({
     __v: {type: Number, select: false},
-    address: {type: String, require: true},
+    address: {type: String, require: true, index: true}, //对地址索引
     blockNumber: {type: Number, require: true},
     blockHash: {type: String, require: true},
-    time: {type: Date, require: true},
+    time: {type: Date, require: true, index: true}, //对时间索引
     transactionHash: {type: String, require: true},
     transactionIndex: {type: Number, require: true},
     topics: {type: Array, require: true},
@@ -15,7 +15,7 @@ const eventSchema = new Schema({
     logIndex: {type: Number, require: true},
     removed: {type: Boolean, require: true},
     returnValues: {type: Object, require: true},
-    event: {type: String, require: true},
+    event: {type: String, require: true, index: true}, //对事件索引
     signature: {type: String, require: true},
     raw: {type: Array, require: true}
 })
