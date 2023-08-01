7e42de0836b41293b821f98a61b647446660a705
jasonzysun <jasonzysun@163.com>
2023-08-01 13:19:41 +0800

    自定义查询条件加入时间范围筛选

--- src/controllers/contract.js
+++ src/controllers/contract.js
@@ -1,7 +1,10 @@
 const Web3 = require('web3').Web3
-const { rpc } = require('../config')
+const {rpc} = require('../config')
 const contractService = require('../service/contract')
-const eventService  = require('../service/event')
+const eventService = require('../service/event')
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc'); // 加载 UTC 插件
+dayjs.extend(utc); // 使用 UTC 插件
 
 class ContractCtl {
     /**
@@ -10,7 +13,7 @@ class ContractCtl {
      */
     async find(ctx) {
         const contracts = await contractService.find()
-        ctx.body = { "contracts": contracts }
+        ctx.body = {"contracts": contracts}
     }
 
     /**
@@ -19,7 +22,9 @@ class ContractCtl {
      */
     async findByAddress(ctx) {
         const contract = await contractService.findByAddress(ctx.params.address)
-        if(!contract) { ctx.throw(404,'No eligible contracts exist') }
+        if (!contract) {
+            ctx.throw(404, 'No eligible contracts exist')
+        }
         ctx.body = contract
     }
 
@@ -34,9 +39,11 @@ class ContractCtl {
             abi: {type: 'array', require: true},
             createdHash: {type: 'string', require: false},
         })
-        const { address, name, abi, createdHash } = ctx.request.body
+        const {address, name, abi, createdHash} = ctx.request.body
         const repeatedContract = await contractService.findByAddress(address)
-        if (repeatedContract) { ctx.throw(409, `Contract:${address} already exists`); }
+        if (repeatedContract) {
+            ctx.throw(409, `Contract:${address} already exists`);
+        }
         // 如果请求中包含了createdHash，进行校验
         if (createdHash) {
             const httpProvider = new Web3.providers.HttpProvider(rpc);
@@ -82,7 +89,9 @@ class ContractCtl {
      */
     async deleteByAddress(ctx) {
         const contract = await contractService.deleteByAddress(ctx.params.address)
-        if(!contract) { ctx.throw(404,'No eligible contracts exist') }
+        if (!contract) {
+            ctx.throw(404, 'No eligible contracts exist')
+        }
         ctx.status = 204
     }
 
@@ -92,7 +101,9 @@ class ContractCtl {
      */
     async startScanning(ctx) {
         const contract = await contractService.startScanning(ctx.params.address)
-        if(!contract) { ctx.throw(404,'No eligible contracts exist') }
+        if (!contract) {
+            ctx.throw(404, 'No eligible contracts exist')
+        }
         ctx.body = contract
     }
 
@@ -103,7 +114,9 @@ class ContractCtl {
      */
     async stopScanning(ctx) {
         const contract = await contractService.stopScanning(ctx.params.address)
-        if(!contract) { ctx.throw(404,'No eligible contracts exist') }
+        if (!contract) {
+            ctx.throw(404, 'No eligible contracts exist')
+        }
         ctx.body = contract
     }
 
@@ -112,9 +125,11 @@ class ContractCtl {
      * @param ctx
      * @return {Promise<void>}
      */
-    async clearEvents(ctx){
+    async clearEvents(ctx) {
         const contract = await contractService.clearEvents(ctx.params.address)
-        if(!contract) { ctx.throw(404,'No eligible contracts exist') }
+        if (!contract) {
+            ctx.throw(404, 'No eligible contracts exist')
+        }
         ctx.body = contract
     }
 
@@ -122,9 +137,8 @@ class ContractCtl {
      * 获取合约的所有事件记录
      * @param ctx
      */
-    async getAllEvents(ctx){
+    async getAllEvents(ctx) {
         const events = await eventService.findByAddress(ctx.params.address)
-        console.log(events)
         ctx.body = events
     }
 
@@ -138,14 +152,19 @@ class ContractCtl {
         })
         const queryCriteria = ctx.request.body.queryCriteria
         queryCriteria.address = queryCriteria.address.toLowerCase()
-        if(!queryCriteria || !queryCriteria.address){
-            ctx.throw(400,'The query criteria must include contract address information. Such as: { address: \'Your_Contract_Address\' }')
+        if (!queryCriteria || !queryCriteria.address) {
+            ctx.throw(400, 'The query criteria must include contract address information. Such as: { address: \'Your_Contract_Address\' }')
+        }
+        if (!queryCriteria.time) {// 原有的查询条件对time没有约束时，加入时间范围条件
+            const endTime = ctx.request.body.endTime ? dayjs.utc(ctx.request.body.endTime).toDate() : dayjs.utc().toDate() //没有提供结束时间时默认使用当前时间
+            const startTime = ctx.request.body.startTime ? dayjs.utc(ctx.request.body.startTime).toDate() : dayjs.utc(endTime).subtract(6, 'day').startOf('day').toDate() //没有提供结束时间时默认使用结束时间前7天的0点时间（也就是默认查询七天的数据）
+            queryCriteria.time = {$gte: startTime, $lte: endTime}
         }
         const selectField = ctx.request.body.selectField ? ctx.request.body.selectField : ''
-        const sortCriteria = ctx.request.body.sortCriteria ? ctx.request.body.sortCriteria : {}
+        const sortCriteria = ctx.request.body.sortCriteria ? ctx.request.body.sortCriteria : {time: -1}
         const page = ctx.request.body.page ? ctx.request.body.page : 1
         const pageSize = ctx.request.body.pageSize ? ctx.request.body.pageSize : 10
-        const events = await eventService.find(queryCriteria,selectField,sortCriteria,page,pageSize)
+        const events = await eventService.find(queryCriteria, selectField, sortCriteria, page, pageSize)
         ctx.body = events
     }
 
