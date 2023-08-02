5d901220c944bb9812342b8dfd189ad8c4922d05
jasonzysun <jasonzysun@163.com>
2023-08-02 11:03:12 +0800

    增加删除合集接口

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,14 @@
+const Contract = require('../models/contract')
+const Event = require('../models/event')
 class HomeCtl {
-    index(ctx){
+    async index(ctx){
         ctx.body = 'Onchain To Offchain Server is Working well!'
+    }
+
+    async dropCollections(ctx){
+        await Contract.collection.drop()
+        await Event.collection.drop()
+        ctx.status = 204
     }
 }
 module.exports = new HomeCtl()
